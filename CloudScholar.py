0087b86e71f0a559598808198e76bfa0a3acb016
Trang TRAN <trandinhcongtrang@gmail.com>
2017-02-09 07:18:55 +0100

    Cloud Agent Setting

--- CloudScholar.py
+++ CloudScholar.py
@@ -250,8 +250,10 @@ class ScholarConf(object):
 
     # USER_AGENT = 'Mozilla/5.0 (X11; U; FreeBSD i386; en-US; rv:1.9.2.9) Gecko/20100913 Firefox/3.6.9'
     # Let's update at this point (3/14):
-    # USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0'
-    USER_AGENT ='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0'
+    
+    # Google Cloud Agent
+    USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0'
+    # USER_AGENT ='Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0'
 
     # If set, we will use this file to read/save cookies to enable
     # cookie use across sessions.
